855b68656f3f2a423a877ec6c4fd218e00e831c1
Marquis <abahmarquis@gmail.com>
2024-11-16 08:39:11 -0800

    update code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,112 +23,118 @@ export default defineNuxtConfig({
         },
         { name: 'mobile-web-app-capable', content: 'yes' },
         { name: 'apple-touch-fullscreen', content: 'yes' },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-  
+        { name: 'apple-mobile-web-app-capable', content: 'yes' }
       ],
       link: [
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2048-2732.jpg",
+          "href": "/icons/apple-splash-dark-2048-2732.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2732-2048.jpg",
+          "href": "/icons/apple-splash-dark-2732-2048.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1668-2388.jpg",
+          "href": "/icons/apple-splash-dark-1668-2388.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2388-1668.jpg",
+          "href": "/icons/apple-splash-dark-2388-1668.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1536-2048.jpg",
+          "href": "/icons/apple-splash-dark-1536-2048.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2048-1536.jpg",
+          "href": "/icons/apple-splash-dark-2048-1536.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1488-2266.jpg",
+          "href": "/icons/apple-splash-dark-1488-2266.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2266-1488.jpg",
+          "href": "/icons/apple-splash-dark-2266-1488.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1640-2360.jpg",
+          "href": "/icons/apple-splash-dark-1640-2360.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2360-1640.jpg",
+          "href": "/icons/apple-splash-dark-2360-1640.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1668-2224.jpg",
+          "href": "/icons/apple-splash-dark-1668-2224.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2224-1668.jpg",
+          "href": "/icons/apple-splash-dark-2224-1668.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1620-2160.jpg",
+          "href": "/icons/apple-splash-dark-1620-2160.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2160-1620.jpg",
+          "href": "/icons/apple-splash-dark-2160-1620.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1320-2868.jpg",
+          "href": "/icons/apple-splash-dark-1320-2868.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2868-1320.jpg",
+          "href": "/icons/apple-splash-dark-2868-1320.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1206-2622.jpg",
+          "href": "/icons/apple-splash-dark-1206-2622.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2622-1206.jpg",
+          "href": "/icons/apple-splash-dark-2622-1206.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-1290-2796.jpg",
+          "href": "/icons/apple-splash-dark-1290-2796.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         },
         {
           "rel": "apple-touch-startup-image",
-          "href": "public/icons/apple-splash-dark-2796-1290.jpg",
+          "href": "/icons/apple-splash-dark-2796-1290.jpg",
           "media": "(prefers-color-scheme: dark) and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
         }
       ],
+      script:[
+        {
+        src:"https://cdn.jsdelivr.net/npm/pwacompat@2.0.6/pwacompat.min.js",
+          integrity:"sha384-GOaSLecPIMCJksN83HLuYf9FToOiQ2Df0+0ntv7ey8zjUHESXhthwvq9hXAZTifA",
+          crossorigin:"anonymous" ,defer:true
+        }
+      ]
     },
   },
   plugins: ["~/plugins/aos.client.ts"],
@@ -140,7 +146,6 @@ export default defineNuxtConfig({
   },
   modules: ["@vite-pwa/nuxt", '@nuxtjs/google-fonts'],
   pwa: {
-    ...require('./public/manifest.json'),
     registerType: "autoUpdate",
     manifest: {
       name: "SWL - Stress Free Weight Loss",
@@ -153,25 +158,25 @@ export default defineNuxtConfig({
       start_url: "/",
       icons: [
         {
-          "src": "public/icons/manifest-icon-192.maskable.png",
+          "src": "/icons/manifest-icon-192.maskable.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "public/icons/manifest-icon-192.maskable.png",
+          "src": "/icons/manifest-icon-192.maskable.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "maskable"
         },
         {
-          "src": "public/icons/manifest-icon-512.maskable.png",
+          "src": "/icons/manifest-icon-512.maskable.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "public/icons/manifest-icon-512.maskable.png",
+          "src": "/icons/manifest-icon-512.maskable.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "maskable"
@@ -215,7 +220,7 @@ export default defineNuxtConfig({
     },
   },
   dir: {
-    static: 'public', // Ensure the public folder is treated as static
+    static: 'public', // Ensure the  folder is treated as static
   },
 
 });
