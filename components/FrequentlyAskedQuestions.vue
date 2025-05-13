5836b1fce3514597e0f55ddc8afb7562b89c75eb
Marquis <abahmarquis@gmail.com>
2025-05-13 17:33:45 +0100

    updated coprrectd trext

--- components/FrequentlyAskedQuestions.vue
+++ components/FrequentlyAskedQuestions.vue
@@ -29,7 +29,7 @@
                 :initial="{ opacity: 0, scale: 0.8 }"
                 :enter="{ opacity: 1, scale: 1, transition: { delay: 600, duration: 800 } }"
               >
-                <img src="/faq-illustration.svg" alt="FAQ Illustration" class="w-full max-w-xs mx-auto" />
+                <!-- <img src="/faq-illustration.svg" alt="FAQ Illustration" class="w-full max-w-xs mx-auto" /> -->
               </div>
             </div>
             
